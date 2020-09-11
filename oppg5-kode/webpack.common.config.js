d2b217e18e89abb1be508886e37fb2e73954191d
Ingrid <32510254+ingridelise96@users.noreply.github.com>
2020-09-12 00:13:39 +0200

    Add files via upload

---
+++ oppg5-kode/webpack.common.config.js
@@ -0,0 +1,28 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/script.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.css'],
+  },
+};
